b4c3c651e563a0aab4f2f32371652514e9e4f988
Filos1992 <max.pargmann@dlr.de>
2024-06-12 12:55:56 +0200

    renamed  split and moved plots to utils

--- 03_create_dataset_hour_split.py
+++ 03_create_dataset_hour_split.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utils import calculate_az_el, plot_stacked_bar_chart, plot_scatter_az_el
+
 # Function to classify time of day
 def classify_time_of_day(hour):
     if 0 <= hour < 12:
@@ -23,14 +25,14 @@ def check_for_empty_sets(grouped):
     print("---------------------")
     return empty_sets
 
-# Function to set DataSet_Azimuth to NaN for specific HeliostatIDs
-def set_azimuth_to_nan(df, heliostats):
-    df.loc[df['HeliostatId'].isin(heliostats), 'DataSet_Azimuth'] = np.nan
+# Function to set DataSet_hour to NaN for specific HeliostatIDs
+def set_hour_to_nan(df, heliostats):
+    df.loc[df['HeliostatId'].isin(heliostats), 'DataSet_hour'] = np.nan
 
 # Updated function to select earliest N entries for each HeliostatId in the 'train' set
 def select_earliest_n_train_entries(df, n):
     # Filter only 'train' entries
-    filtered_df_train = df[df['DataSet_Azimuth'] == 'train']
+    filtered_df_train = df[df['DataSet_hour'] == 'train']
     
     # Group by 'HeliostatId' and filter groups with at least n entries
     valid_heliostats = filtered_df_train.groupby('HeliostatId').filter(lambda x: len(x) >= n)['HeliostatId'].unique()
@@ -68,7 +70,7 @@ def plot_stacked_bar_chart(grouped_df, train_split_name):
     plt.tight_layout()  # Adjust layout to prevent clipping of labels
     plt.show()  # Show the plot
 
-    plt.savefig(f"03_Azimuth_Split_{train_split_name}.png", dpi=300)
+    plt.savefig(f"03_hour_split_{train_split_name}.png", dpi=300)
 
 # Load CSV
 pathDF = 'data/DatenHeliOS/calib_data.csv'
@@ -78,48 +80,59 @@ df = pd.read_csv(pathDF).set_index('id')  # Set df id as index
 df['CreatedAt'] = pd.to_datetime(df['CreatedAt'])
 
 # Apply the function to create the new column
-df['DataSet_Azimuth'] = df['CreatedAt'].dt.hour.apply(classify_time_of_day)
+df['DataSet_hour'] = df['CreatedAt'].dt.hour.apply(classify_time_of_day)
 
-# Group by 'HeliostatId' and 'DataSet_Azimuth' and count the occurrences
-grouped = df.dropna(subset=['DataSet_Azimuth']).groupby(['HeliostatId', 'DataSet_Azimuth']).size().unstack(fill_value=0)
+# Calculate Azimuth and Elevation
+df['Azimuth'], df['Elevation'] = calculate_az_el(df)
+
+# Group by 'HeliostatId' and 'DataSet_hour' and count the occurrences
+grouped = df.dropna(subset=['DataSet_hour']).groupby(['HeliostatId', 'DataSet_hour']).size().unstack(fill_value=0)
 plot_stacked_bar_chart(grouped, train_split_name="unfiltered")
 
 # Check for empty sets
 empty_sets = check_for_empty_sets(grouped)
 
-# Combine all HeliostatIDs that need DataSet_Azimuth set to NaN
+# Combine all HeliostatIDs that need DataSet_hour set to NaN
 heliostats_to_nan = set().union(*empty_sets.values())
-df.loc[df['HeliostatId'].isin(heliostats_to_nan), 'DataSet_Azimuth'] = np.nan  # Remove empty sets
+df.loc[df['HeliostatId'].isin(heliostats_to_nan), 'DataSet_hour'] = np.nan  # Remove empty sets
 
 # Update grouped dataframe after removing empty sets
-grouped = df.dropna(subset=['DataSet_Azimuth']).groupby(['HeliostatId', 'DataSet_Azimuth']).size().unstack(fill_value=0)
+grouped = df.dropna(subset=['DataSet_hour']).groupby(['HeliostatId', 'DataSet_hour']).size().unstack(fill_value=0)
 plot_stacked_bar_chart(grouped, train_split_name="no_empty_sets")
 
 # Create separate columns for different N
-df['DataSet_Azimuth_10'] = df['DataSet_Azimuth']
-df['DataSet_Azimuth_50'] = df['DataSet_Azimuth']
-df['DataSet_Azimuth_100'] = df['DataSet_Azimuth']
+df['DataSet_hour_10'] = df['DataSet_hour']
+df['DataSet_hour_50'] = df['DataSet_hour']
+df['DataSet_hour_100'] = df['DataSet_hour']
 
 # Select indices of earliest N entries
 earliest_10_indices = select_earliest_n_train_entries(df, 10)
 earliest_50_indices = select_earliest_n_train_entries(df, 50)
 earliest_100_indices = select_earliest_n_train_entries(df, 100)
 
-# Set 'DataSet_Azimuth' to NaN for 'train' entries not in the earliest N indices
-df.loc[(df['DataSet_Azimuth_10'] == 'train') & (~df.index.isin(earliest_10_indices)), 'DataSet_Azimuth_10'] = np.nan
-df.loc[(df['DataSet_Azimuth_50'] == 'train') & (~df.index.isin(earliest_50_indices)), 'DataSet_Azimuth_50'] = np.nan
-df.loc[(df['DataSet_Azimuth_100'] == 'train') & (~df.index.isin(earliest_100_indices)), 'DataSet_Azimuth_100'] = np.nan
+# Set 'DataSet_hour' to NaN for 'train' entries not in the earliest N indices
+df.loc[(df['DataSet_hour_10'] == 'train') & (~df.index.isin(earliest_10_indices)), 'DataSet_hour_10'] = np.nan
+df.loc[(df['DataSet_hour_50'] == 'train') & (~df.index.isin(earliest_50_indices)), 'DataSet_hour_50'] = np.nan
+df.loc[(df['DataSet_hour_100'] == 'train') & (~df.index.isin(earliest_100_indices)), 'DataSet_hour_100'] = np.nan
 
 # Plot updated grouped data, ensuring 'train' set data is present
-grouped = df.dropna(subset=['DataSet_Azimuth_10']).groupby(['HeliostatId', 'DataSet_Azimuth_10']).size().unstack(fill_value=0)
+grouped = df.dropna(subset=['DataSet_hour_10']).groupby(['HeliostatId', 'DataSet_hour_10']).size().unstack(fill_value=0)
 plot_stacked_bar_chart(grouped, train_split_name="10")
 
-grouped = df.dropna(subset=['DataSet_Azimuth_50']).groupby(['HeliostatId', 'DataSet_Azimuth_50']).size().unstack(fill_value=0)
+grouped = df.dropna(subset=['DataSet_hour_50']).groupby(['HeliostatId', 'DataSet_hour_50']).size().unstack(fill_value=0)
 plot_stacked_bar_chart(grouped, train_split_name="50")
 
-grouped = df.dropna(subset=['DataSet_Azimuth_100']).groupby(['HeliostatId', 'DataSet_Azimuth_100']).size().unstack(fill_value=0)
+grouped = df.dropna(subset=['DataSet_hour_100']).groupby(['HeliostatId', 'DataSet_hour_100']).size().unstack(fill_value=0)
 plot_stacked_bar_chart(grouped, train_split_name="100")
 
+# Filter for a specific HeliostatID (change 'example_heliostat_id' to your specific ID)
+filtered_df = df.dropna(subset=['DataSet_hour_50'])
+example_heliostat_id = filtered_df['HeliostatId'].iloc[1]  # Get a specific HeliostatId
+single_heliostat_df = filtered_df[filtered_df['HeliostatId'] == example_heliostat_id]
+
+# Call the plot function
+plot_scatter_az_el(single_heliostat_df, "DataSet_hour", "03_hour_split_example_heliostat.png")
+
 # Save the final dataframe
-df.to_csv('Azimuth_Split.csv', header=True)
+df.to_csv('03_hour_split.csv', header=True)
 print(df.head(100))
