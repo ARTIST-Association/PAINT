df126e1743919612784b627cd5d60306f36feda7
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-07-05 15:37:15 +0200

    refactor, include docstrings, include type hints, make executable

--- plots/03_create_dataset_hour_split.py
+++ plots/03_create_dataset_hour_split.py
@@ -1,192 +1,218 @@
-import pandas as pd
-import numpy as np
+import argparse
+from pathlib import Path
+from typing import List, Union
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import pandas as pd
+from general_plotting_functions import (
+    mark_insufficient_data_as_nan,
+    plot_stacked_bar_chart_with_inset,
+)
 
-from paint.util.utils import calculate_az_el
-
-
-# Function to classify time of day
-def classify_azimuth_split(df, split_name, head, tail):
-    df = df.sort_values(
-        by=["Azimuth", "CreatedAt"]
-    )  # Ensure sorted by azimuth and timestamp
-    train_indices = df.head(head).index
-    validation_indices = df.tail(tail).index
-    df[split_name] = "test"  # Default to 'test'
-    df.loc[train_indices, split_name] = "train"
-    df.loc[validation_indices, split_name] = "validation"
-    return df[split_name]
+import paint.util.paint_mappings as mappings
+from paint import PAINT_ROOT
+from paint.util.utils import calculate_azimuth_and_elevation
 
 
-def plot_stacked_bar_chart_with_inset(
-    grouped_df,
-    example_heliostat_df,
-    train_split_name,
-    ax=None,
-    show_legend=False,
-    show_ylabel=False,
-):
+class DatasetHourSplit:
     """
-    Compute the total measurements for each HeliostatID and plot a stacked bar chart with inset scatter plot.
+    Create hour splits of the dataset.
 
-    Parameters:
-    - grouped_df: DataFrame containing measurements grouped by HeliostatID
-    - example_heliostat_df: DataFrame containing example heliostat data
-    - train_split_name: Name of the training split
-    - ax: Axes object to plot on. If None, a new figure will be created.
-    - show_legend: Whether to show the legend (default is False)
+    Attributes
+    ----------
+    measurements_df : pd.DataFrame
+        The measured data.
+    output_path : Union[Path, str]
+        The path to the output directory to save the plots.
+    number_of_train_samples : List[int]
+        The number of samples used for training.
+    number_of_validation_samples : List[int]
+        The number of samples used for validation.
+    file_name : str
+        The file name to save the plot.
 
-    Returns:
-    None
+    Methods
+    -------
+    load_data()
+        Load the data and include additional features.
+    classify_azimuth_split()
+        Classify the data according to the azimuth split.
+    plot_hour_split()
+        Plot the hour split.
     """
-    # Compute the total measurements for each HeliostatID
-    grouped_df["Total"] = grouped_df.sum(axis=1)
 
-    # Sort by total measurements for clearer plotting
-    grouped_df = grouped_df.sort_values("Total")
+    def __init__(
+        self,
+        path_to_measurements: Union[Path, str],
+        output_path: Union[str, Path],
+        number_of_train_samples: List[int],
+        number_of_validation_samples: int,
+        default_example_heliostat: int = 11447,
+        file_name: str = "03_combined_plots",
+        save_as_pdf: bool = True,
+    ) -> None:
+        """
+        Initialize the hour dataset splitter.
 
-    bar_width = 2  # Bar width
+        Parameters
+        ----------
+        path_to_measurements : Union[Path, str]
+            The path to the measurement data.
+        output_path : Union[Path, str]
+            The path to the output directory to save the plots.
+        number_of_train_samples : List[int]
+            The number of samples used for training.
+        number_of_validation_samples : List[int]
+            The number of samples used for validation.
+        file_name : str
+            The file name to save the plot.
+        save_as_pdf : bool
+            Indicates if the output should be saved as PDF (Default: True).
+        """
+        self.measurements_df = self.load_data(Path(path_to_measurements))
+        self.output_path = Path(output_path)
+        if not self.output_path.is_dir():
+            self.output_path.mkdir(parents=True, exist_ok=True)
+        self.number_of_train_samples = number_of_train_samples
+        self.number_of_validation_samples = number_of_validation_samples
+        self.default_example_heliostat = default_example_heliostat
+        if save_as_pdf:
+            self.file_name = file_name + ".pdf"
+        else:
+            self.file_name = file_name + ".png"
 
-    if ax is None:
-        _, ax = plt.subplots(figsize=(10, 6))  # Create figure and axis objects
+    @staticmethod
+    def load_data(path_to_measurements: Path) -> pd.DataFrame:
+        """
+        Load the data and include additional features.
 
-    # Plot each layer of the stacked bar chart
-    ax.bar(
-        grouped_df["Total"],
-        grouped_df["train"],
-        width=bar_width,
-        label="Train",
-        color="skyblue",
-    )
-    ax.bar(
-        grouped_df["Total"],
-        grouped_df["test"],
-        width=bar_width,
-        label="Test",
-        color="salmon",
-        bottom=grouped_df["train"],
-    )
-    ax.bar(
-        grouped_df["Total"],
-        grouped_df["validation"],
-        width=bar_width,
-        label="Validation",
-        color="lightgreen",
-        bottom=grouped_df["train"] + grouped_df["test"],
-    )
+        Parameters
+        ----------
+        path_to_measurements : Path
+            The path to the measurement data.
 
-    # Customize the plot
-    ax.set_xlabel("# Measurements per Heliostat")
-    ax.set_ylabel("# Measurements per Heliostat")
+        Returns
+        -------
+        pd.DataFrame
+            The loaded data with additional time features.
+        """
+        df = pd.read_csv(path_to_measurements).set_index(
+            mappings.ID_INDEX
+        )  # Set df id as index
+        df[mappings.CREATED_AT] = pd.to_datetime(df[mappings.CREATED_AT])
+        df[mappings.AZIMUTH], df[mappings.ELEVATION] = calculate_azimuth_and_elevation(
+            df
+        )  # Calculate Azimuth and Elevation from Sun Vector
+        return df
 
-    ax.set_xlim(
-        0, 610
-    )  # Ensure x-axis starts at 0 and ends at the number of heliostats
-    ax.set_ylim(
-        0, 610
-    )  # Ensure y-axis starts at 0 and ends at the number of heliostats
+    @staticmethod
+    def classify_azimuth_split(
+        df: pd.DataFrame,
+        split_name: str,
+        head: int,
+        tail: int,
+    ) -> pd.DataFrame:
+        """
+        Classify the azimuth split in the data.
 
-    if show_legend:
-        ax.legend(bbox_to_anchor=(0.3, 1.05), loc="center left", ncol=3)
+        Parameters
+        ----------
+        df : pd.DataFrame
+            The data to be classified.
+        split_name : str
+            The name of the split used.
+        head : int
+            The head values to consider.
+        tail : int
+            The tail values to consider.
 
-    if show_ylabel:
-        ax.set_ylabel("# Measurements per Heliostat")
-    else:
-        ax.set_ylabel(None)  # Remove y-label
-    # Create an inset for the scatter plot
-    ax_inset = inset_axes(
-        ax,
-        width="95%",
-        height="95%",
-        bbox_to_anchor=(0.05, 0.45, 0.4, 0.5),
-        loc="upper left",
-        bbox_transform=ax.transAxes,
-    )
+        Returns
+        -------
+        pd.DataFrame
+            The data classified by the azimuth split.
+        """
+        df = df.sort_values(
+            by=[mappings.AZIMUTH, mappings.CREATED_AT]
+        )  # Ensure sorted by azimuth and timestamp
+        train_indices = df.head(head).index
+        validation_indices = df.tail(tail).index
+        df[split_name] = mappings.TEST_INDEX  # Default to 'test'
+        df.loc[train_indices, split_name] = "train"
+        df.loc[validation_indices, split_name] = "validation"
+        return df[split_name]
 
-    colors = {"train": "blue", "test": "red", "validation": "green"}
-    for label, color in colors.items():
-        subset = example_heliostat_df[example_heliostat_df[train_split_name] == label]
-        ax_inset.scatter(
-            subset["Azimuth"], subset["Elevation"], label=label, color=color, alpha=0.5
+    def plot_hour_split(self):
+        """Plot the hour split."""
+        fig, axes = plt.subplots(
+            1, len(self.number_of_train_samples), figsize=(32, 6), sharey=True
         )
-    ax_inset.set_xlabel("Azimuth")
-    ax_inset.set_ylabel("Elevation")
-    ax_inset.set_title("Example Heliostat")
+
+        for i, n in enumerate(self.number_of_train_samples):
+            self.measurements_df[
+                f"{mappings.DATA_SET_AZIMUTH}_{n}"
+            ] = self.measurements_df.groupby(
+                mappings.HELIOSTAT_ID, group_keys=False
+            ).apply(
+                lambda x: self.classify_azimuth_split(
+                    x,
+                    f"{mappings.DATA_SET_AZIMUTH}_{n}",
+                    n,
+                    self.number_of_validation_samples,
+                ),
+                include_groups=False,
+            )
+            df = mark_insufficient_data_as_nan(
+                self.measurements_df,
+                f"{mappings.DATA_SET_AZIMUTH}_{n}",
+                n,
+                self.number_of_validation_samples,
+            )
+            split_counts_by_heliostat = (
+                df.dropna(subset=[f"{mappings.DATA_SET_AZIMUTH}_{n}"])
+                .groupby([mappings.HELIOSTAT_ID, f"{mappings.DATA_SET_AZIMUTH}_{n}"])
+                .size()
+                .unstack(fill_value=0)
+            )
+            example_heliostat_df = df[
+                df[mappings.HELIOSTAT_ID] == self.default_example_heliostat
+            ]  # arbitrary heliostat
+            plot_stacked_bar_chart_with_inset(
+                split_counts_by_heliostat,
+                example_heliostat_df,
+                f"{mappings.DATA_SET_AZIMUTH}_{n}",
+                ax=axes[i],
+                show_legend=(i == 1),  # Show legend only for the first subplot
+                show_y_label=(i == 0),
+            )
+        plt.tight_layout()
+        plt.savefig(self.output_path / self.file_name, dpi=300)
 
 
-def update_datasets_to_nan_if_too_small(df, column, n_train, m_validation):
-    """
-    Update the dataset split to set 'NaN' for heliostats where 'train' set is smaller than n_train
-    and 'test' set is smaller than m_validation.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-    Parameters:
-    df (pd.DataFrame): DataFrame with the dataset split column.
-    column (str): Column name to check for 'train' and 'test' entries.
-    n_train (int): Minimum number of 'train' entries required for each HeliostatId.
-    m_validation (int): Minimum number of 'test' entries required for each HeliostatId.
-
-    Returns:
-    pd.DataFrame: Updated DataFrame with adjusted dataset split.
-    """
-    # Group by HeliostatId and count the number of 'train' and 'test' entries
-    counts = df.groupby("HeliostatId")[column].value_counts().unstack(fill_value=0)
-
-    # Identify HeliostatIds with 'train' set smaller than n_train
-    insufficient_train_heliostats = counts[counts.get("train", 0) < n_train].index
-
-    # Identify HeliostatIds with 'test' set smaller than m_validation
-    insufficient_test_heliostats = counts[counts.get("test", 0) < m_validation].index
-
-    # Combine both sets of insufficient HeliostatIds
-    insufficient_heliostats = set(insufficient_train_heliostats).union(
-        insufficient_test_heliostats
+    parser.add_argument(
+        "--path_to_measurements",
+        type=str,
+        default="data/DatenHeliOS/calib_data.csv",
     )
-
-    # Update the dataset split for those HeliostatIds
-    df.loc[df["HeliostatId"].isin(insufficient_heliostats), column] = np.nan
-
-    return df
-
-
-# Load CSV and prepare columns
-pathdf = "data/DatenHeliOS/calib_data.csv"
-df = pd.read_csv(pathdf).set_index("id")  # Set df id as index
-df["CreatedAt"] = pd.to_datetime(df["CreatedAt"])
-df["Azimuth"], df["Elevation"] = calculate_az_el(
-    df
-)  # Calculate Azimuth and Elevation from Sun Vector
-
-# Apply classification function
-n_train = [10, 50, 100]  # number of train samples per HeliostatId
-m_validation = 30  # number of validation samples per HeliostatId
-
-
-fig, axes = plt.subplots(1, len(n_train), figsize=(32, 6), sharey=True)
-
-for i, n in enumerate(n_train):
-    df[f"DataSet_Azimuth_{n}"] = df.groupby("HeliostatId", group_keys=False).apply(
-        lambda x: classify_azimuth_split(x, f"DataSet_Azimuth_{n}", n, m_validation), include_groups=False)
-    df = update_datasets_to_nan_if_too_small(
-        df, f"DataSet_Azimuth_{n}", n, m_validation
+    parser.add_argument(
+        "--output_path", type=str, default=f"{PAINT_ROOT}/plots/saved_plots"
     )
-    split_counts_by_heliostat = (
-        df.dropna(subset=[f"DataSet_Azimuth_{n}"])
-        .groupby(["HeliostatId", f"DataSet_Azimuth_{n}"])
-        .size()
-        .unstack(fill_value=0)
-    )
-    example_heliostat_df = df[df["HeliostatId"] == 11447]  # arbitrary heliostat
-    plot_stacked_bar_chart_with_inset(
-        split_counts_by_heliostat,
-        example_heliostat_df,
-        f"DataSet_Azimuth_{n}",
-        ax=axes[i],
-        show_legend=(i == 1),  # Show legend only for the first subplot
-        show_ylabel=(i == 0),
-    )
-plt.tight_layout()
+    parser.add_argument("--number_of_train_samples", default=[10, 50, 100])
+    parser.add_argument("--number_of_validation_samples", default=30)
+    parser.add_argument("--default_example_heliostat", type=int, default=11447)
+    parser.add_argument("--file_name", type=str, default="03_combined_plots")
+    parser.add_argument("--save_as_pdf", action="store_true", default=True)
+    args = parser.parse_args()
 
-plt.savefig("03_combined_plots.png", dpi=300)
-plt.savefig("03_combined_plots.pdf", dpi=300)
-plt.show()
+    plotter = DatasetHourSplit(
+        path_to_measurements=args.path_to_measurements,
+        output_path=args.output_path,
+        number_of_train_samples=args.number_of_train_samples,
+        number_of_validation_samples=args.number_of_validation_samples,
+        default_example_heliostat=args.default_example_heliostat,
+        file_name=args.file_name,
+        save_as_pdf=args.save_as_pdf,
+    )
+    plotter.plot_hour_split()
