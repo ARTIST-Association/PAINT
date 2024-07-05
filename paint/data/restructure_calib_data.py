c73a54003ef7b25c32aa03723fafa5df70553a77
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-07-05 15:36:18 +0200

    comment out code - not sure what this is for

--- paint/data/restructure_calib_data.py
+++ paint/data/restructure_calib_data.py
@@ -1,18 +1,32 @@
-import pandas as pd
-from paint.util.utils import num_to_name
-measurement_path = 'data/DatenHeliOS/calib_data.csv'
-df_measurements = pd.read_csv(measurement_path)
-df_measurements = df_measurements.set_index('id') # Set df id as index
-# Get all existing heliostat IDs and their entry counts
-heliostat_counts = df_measurements['HeliostatId'].value_counts()
-# Replace HeliostatId with heliostat names in heliostat_counts dataframe
-heliostat_counts.index = heliostat_counts.index.map(num_to_name)
+# TODO: Find out why this code is here!
 
-#remove unneeded columns
-#Field ID always 1, Camera ID always 0 , 
-# System always "HeliOS", Version always 1, 
-# LastScore and GeometryData are fitted, 
-# UpdatedAt same time as CreatedAt
-df_measurements = df_measurements.drop(columns=['FieldId', 'CameraId', "System","Version", "LastScore", "GeometryData", "UpdatedAt"])
+# import pandas as pd
+#
+# from paint.util.utils import heliostat_id_to_heliostat_name
+#
+# measurement_path = "data/DatenHeliOS/calib_data.csv"
+# df_measurements = pd.read_csv(measurement_path)
+# df_measurements = df_measurements.set_index("id")  # Set df id as index
+# # Get all existing heliostat IDs and their entry counts
+# heliostat_counts = df_measurements["HeliostatId"].value_counts()
+# # Replace HeliostatId with heliostat names in heliostat_counts dataframe
+# heliostat_counts.index = heliostat_counts.index.map(heliostat_id_to_heliostat_name)
+#
+# # remove unneeded columns
+# # Field ID always 1, Camera ID always 0 ,
+# # System always "HeliOS", Version always 1,
+# # LastScore and GeometryData are fitted,
+# # UpdatedAt same time as CreatedAt
+# df_measurements = df_measurements.drop(
+#     columns=[
+#         "FieldId",
+#         "CameraId",
+#         "System",
+#         "Version",
+#         "LastScore",
+#         "GeometryData",
+#         "UpdatedAt",
+#     ]
+# )
 
-#TODO: Add column for Calibration Image and and Target
+# TODO: Add column for Calibration Image and and Target
