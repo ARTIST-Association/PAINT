5bcfae4c677aca523fbb3e46196c390b692e73b5
Moritz Leibauer <moritz.leibauer@synhelion.com>
2024-07-11 23:14:28 +0200

    Add Target Detection Test - Adds a test programm to run target detections. - Testing data is currently not provided! - Template matching still fails!

---
+++ tests/target_cropper/test_target_detection.py
@@ -0,0 +1,61 @@
+import torch
+
+import sys
+import os
+
+lib_dir = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
+sys.path.append(lib_dir)
+from paint import target_cropper
+
+
+# TODO make real test
+def main():
+    image = target_cropper.util.load_image(
+        os.path.join(__file__, os.pardir, "uncropped_image.png")
+    )
+    target = target_cropper.dataclasses.Target(
+        marker_1=target_cropper.dataclasses.Marker(
+            image_position=torch.tensor([200, 0]),
+            template_offset=torch.tensor([0, 0]),
+            enu_position=torch.tensor([0, 0, 1]),
+            template_image=target_cropper.util.load_image(
+                os.path.join(__file__, os.pardir, "templates", "upper_center.png")
+            ),
+        ),
+        marker_2=target_cropper.dataclasses.Marker(
+            image_position=torch.tensor([400, 200]),
+            template_offset=torch.tensor([0, 0]),
+            enu_position=torch.tensor([1, 0, 0]),
+            template_image=target_cropper.util.load_image(
+                os.path.join(__file__, os.pardir, "templates", "center_right.png")
+            ),
+        ),
+        marker_3=target_cropper.dataclasses.Marker(
+            image_position=torch.tensor([200, 400]),
+            template_offset=torch.tensor([0, 0]),
+            enu_position=torch.tensor([0, 0, -1]),
+            template_image=target_cropper.util.load_image(
+                os.path.join(__file__, os.pardir, "templates", "lower_center.png")
+            ),
+        ),
+        marker_4=target_cropper.dataclasses.Marker(
+            image_position=torch.tensor([0, 200]),
+            template_offset=torch.tensor([0, 0]),
+            enu_position=torch.tensor([-1, 0, 0]),
+            template_image=target_cropper.util.load_image(
+                os.path.join(__file__, os.pardir, "templates", "center_left.png")
+            ),
+        ),
+        output_shape=torch.Size([400,400])
+    )
+
+    # warp image
+    warped_image = target_cropper.detect_target(image=image, target=target)
+
+    # Convert the tensor to a PIL image and display it
+    warped_image_pil = target_cropper.util.to_image(warped_image)
+    warped_image_pil.show()
+
+
+if __name__ == "__main__":
+    main()
