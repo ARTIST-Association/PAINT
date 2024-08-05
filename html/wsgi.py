d4b3d145077d26f359d2d583c36fd0ee3dce4834
Markus Goetz <markus.goetz@kit.edu>
2024-08-05 20:49:15 +0200

    Added subpages

--- html/wsgi.py
+++ html/wsgi.py
@@ -15,7 +15,59 @@ def home() -> str:
     str
         The rendered template of the main landing page
     """
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+@app.route("/data")
+def data() -> str:
+    """
+    Register the controller for the data page.
+
+    Returns
+    -------
+    str
+        The rendered template of the main landing page
+    """
+    return render_template("data.html")
+
+
+@app.route("/papers")
+def papers() -> str:
+    """
+    Register the controller for the papers page.
+
+    Returns
+    -------
+    str
+        The rendered template of the main landing page
+    """
+    return render_template("papers.html")
+
+
+@app.route("/partners")
+def partners() -> str:
+    """
+    Register the controller for the partners page.
+
+    Returns
+    -------
+    str
+        The rendered template of the main landing page
+    """
+    return render_template("partners.html")
+
+
+@app.route("/resources")
+def resources() -> str:
+    """
+    Register the controller for the resources page.
+
+    Returns
+    -------
+    str
+        The rendered template of the main landing page
+    """
+    return render_template("resources.html")
 
 
 if __name__ == "__main__":
