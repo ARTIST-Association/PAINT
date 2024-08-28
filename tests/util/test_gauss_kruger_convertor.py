0c68a7a14d57ef2236b3fa1cf364151bafe0028d
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-08-28 11:46:23 +0200

    include test for the Gauss-Kruger convertor

---
+++ tests/util/test_gauss_kruger_convertor.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+
+import pytest
+
+from paint.util.gauss_kruger_convertor import convert_gk_to_lat_long
+
+
+@pytest.mark.parametrize(
+    "right, height, expected",
+    [
+        (2527321, 5642083, (50.91338551710208, 6.387849990173632)),
+        (2587321, 5692083, (51.35676539020598, 7.252958250006098)),
+    ],
+)
+def test_gauss_kruger_convertor(
+    right: float, height: float, expected: Tuple[float, float]
+) -> None:
+    """
+    Test the Gauss-Kruger convertor.
+
+    Parameters
+    ----------
+    right : float
+        The original right coordinate.
+    height : float
+        The original height coordinate.
+    expected : Tuple[float, float
+        The expected converted latitude and longitude coordinate.
+    """
+    actual = convert_gk_to_lat_long(right, height)
+    assert actual == expected
