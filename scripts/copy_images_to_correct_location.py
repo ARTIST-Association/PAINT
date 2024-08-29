e603be66764084db371585b6ab4ac72cd4682275
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-08-29 11:18:58 +0200

    update script to deal with changed names and run on LSDF

--- scripts/copy_images_to_correct_location.py
+++ scripts/copy_images_to_correct_location.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
+import csv
 import os
 import shutil
 import sys
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 
@@ -16,9 +18,11 @@ from paint.util.utils import (
     to_utc,
 )
 
+MISSED_FILES: List = []
+
 
 def find_and_copy_file(
-    source_directory: Path, file_name: str, destination_path_and_name: Path
+    source_directory: Path, id_str: str, destination_path_and_name: Path
 ) -> None:
     """
     Find a given file within a directory or sub directory and copy it to a desired destination.
@@ -27,29 +31,31 @@ def find_and_copy_file(
     ----------
     source_directory : Path
         The source directory to begin the search for the file.
-    file_name
-        The file name being searched for.
+    id_str
+        The ID string being searched for.
     destination_path_and_name
         The full destination path and file name used for copying the file.
     """
     # Walk through all subdirectories of the source directory
     for root, dirs, files in os.walk(source_directory):
-        if file_name in files:
-            # Get the full path of the file
-            file_path = os.path.join(root, file_name)
+        for file_name in files:
+            if file_name == f"{id_str}.png" or file_name.startswith(f"{id_str}_"):
+                # Get the full path of the file
+                file_path = os.path.join(root, file_name)
 
-            # Ensure the destination directory exists
-            destination_path_and_name.parent.mkdir(parents=True, exist_ok=True)
+                # Ensure the destination directory exists
+                destination_path_and_name.parent.mkdir(parents=True, exist_ok=True)
 
-            # Copy the file to the destination directory
-            shutil.copy(file_path, destination_path_and_name)
+                # Copy the file to the destination directory
+                shutil.copy(file_path, destination_path_and_name)
 
-            print(
-                f"File '{file_name}' found and copied to '{destination_path_and_name}'."
-            )
-            return
+                print(
+                    f"File '{file_name}' found and copied to '{destination_path_and_name}'."
+                )
+                return
 
-    print(f"File '{file_name}' not found in '{source_directory}'.")
+    print(f"The ID '{id_str}' was not found in '{source_directory}'.")
+    MISSED_FILES.append(id_str)
 
 
 def main(arguments: argparse.Namespace) -> None:
@@ -80,30 +86,42 @@ def main(arguments: argparse.Namespace) -> None:
     for heliostat, heliostat_data in data.groupby(mappings.HELIOSTAT_ID):
         for index in heliostat_data.index:
             assert isinstance(heliostat, str)
-            file_name = str(index) + ".png"
+            id_string = str(index)
             destination_path = (
                 Path(arguments.output_path)
                 / heliostat
                 / mappings.SAVE_CALIBRATION
-                / file_name
+                / (id_string + ".png")
             )
             find_and_copy_file(
                 source_directory=source,
-                file_name=file_name,
+                id_str=id_string,
                 destination_path_and_name=destination_path,
             )
+    saved_missed_ids_path = Path(f"{PAINT_ROOT}/MISSED_IDS/missed_ids.csv")
+    saved_missed_ids_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(saved_missed_ids_path, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        for item in MISSED_FILES:
+            writer.writerow([item])
 
 
 if __name__ == "__main__":
+    lsdf_root = os.environ.get("LSDFPROJECTS")
+    assert isinstance(lsdf_root, str)
+    input_folder = Path(lsdf_root) / "paint" / "PAINT" / "CalibrationDataRaw"
+    output_folder = Path(lsdf_root) / "paint" / mappings.POWER_PLANT_GPPD_ID
+    input_calibration = Path(input_folder) / "paint" / "PAINT" / "calib_data.csv"
     # Simulate command-line arguments for testing or direct script execution
     sys.argv = [
         "copy_images_to_correct_location.py",
         "--input_folder",
-        f"{PAINT_ROOT}/ExampleDataKIT",
+        str(input_folder),
         "--output_path",
-        f"{PAINT_ROOT}/ConvertedData",
+        str(output_folder),
         "--input_calibration",
-        f"{PAINT_ROOT}/ExampleDataKIT/dataframe.csv",
+        str(input_calibration),
     ]
 
     parser = argparse.ArgumentParser()
@@ -118,7 +136,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input_calibration",
         type=Path,
-        default=f"{PAINT_ROOT}/ExampleDataKIT/dataframe.csv",
     )
     args = parser.parse_args()
     main(arguments=args)
