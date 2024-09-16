c72b45b8ff707f74925409c196bec782d228292a
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-09-16 16:30:39 +0200

    split juelich weather up per month

--- scripts/merge_juelich_weather_and_generate_stac.py
+++ scripts/merge_juelich_weather_and_generate_stac.py
@@ -47,26 +47,29 @@ def main(arguments: argparse.Namespace) -> None:
     weather_converter = JuelichWeatherConverter(
         input_root_dir=arguments.input_root_dir,
         output_path=arguments.output_path,
-        file_name=arguments.file_name,
     )
-    metadata = weather_converter.merge_and_save_to_hdf5()
+    metadata_df = weather_converter.merge_and_save_to_hdf5()
 
-    juelich_stac = make_juelich_weather_item(data=metadata)
-    save_path = Path(arguments.output_path) / (mappings.JUELICH_STAC_NAME + ".json")
-    save_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(save_path, "w") as handle:
-        json.dump(juelich_stac, handle)
+    for group_name, metadata in metadata_df.iterrows():
+        assert isinstance(group_name, str)
+        juelich_stac = make_juelich_weather_item(data=metadata, month_group=group_name)
+        save_path = Path(arguments.output_path) / (
+            mappings.JUELICH_STAC_NAME % group_name + ".json"
+        )
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(save_path, "w") as handle:
+            json.dump(juelich_stac, handle)
 
-    weather_items.loc[len(weather_items)] = [
-        "Juelich weather data",
-        mappings.JUELICH_STAC_URL,
-        metadata[mappings.JUELICH_START],
-        metadata[mappings.JUELICH_END],
-        mappings.JUELICH_WEATHER_LAT,
-        mappings.JEULICH_WEATHER_LON,
-        mappings.JEULICH_WEATHER_ALTITUDE,
-    ]
-    weather_items.to_csv(weather_items_path, index=False)
+        weather_items.loc[len(weather_items)] = [
+            f"Juelich weather data for {group_name}",
+            mappings.JUELICH_STAC_URL % group_name,
+            metadata[mappings.JUELICH_START],
+            metadata[mappings.JUELICH_END],
+            mappings.JUELICH_WEATHER_LAT,
+            mappings.JUELICH_WEATHER_LON,
+            mappings.JUELICH_WEATHER_ALTITUDE,
+        ]
+        weather_items.to_csv(weather_items_path, index=False)
 
 
 if __name__ == "__main__":
@@ -79,6 +82,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_root_dir", type=str, default=str(input_folder))
     parser.add_argument("--output_path", type=str, default=str(output_folder))
-    parser.add_argument("--file_name", type=str, default="juelich-weather.h5")
     args = parser.parse_args()
     main(arguments=args)
