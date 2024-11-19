1e18642a4cb1c6fc4261a0423b61ca0bd32effc9
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-11-19 10:10:39 +0100

    adjust coordinate conversion for focal spots

--- preprocessing-scripts/generate_calibration_stacs.py
+++ preprocessing-scripts/generate_calibration_stacs.py
@@ -14,9 +14,9 @@ from paint.preprocessing.calibration_stac import (
     make_calibration_collection,
     make_calibration_item,
 )
-from paint.util import convert_gk_to_lat_long
 from paint.util.utils import (
     calculate_azimuth_and_elevation,
+    convert_field_coordinate_to_wgs84,
     heliostat_id_to_name,
     to_utc,
 )
@@ -154,14 +154,17 @@ def main(arguments: argparse.Namespace) -> None:
             json.dump(stac_item, handle)
 
         # Save associated calibration properties.
-        focal_spot_lat, focal_spot_lon = convert_gk_to_lat_long(
-            right=mappings.GK_RIGHT_BASE + heliostat_data[mappings.TARGET_OFFSET_E],
-            height=mappings.GK_HEIGHT_BASE + heliostat_data[mappings.TARGET_OFFSET_N],
+        (
+            focal_spot_lat,
+            focal_spot_lon,
+        ) = convert_field_coordinate_to_wgs84(
+            east_coordinate_m=heliostat_data[mappings.TARGET_OFFSET_E],
+            north_coordinate_m=heliostat_data[mappings.TARGET_OFFSET_N],
         )
         if processed_available:
-            utis_lat, utis_long = convert_gk_to_lat_long(
-                right=mappings.GK_RIGHT_BASE + utis_data.loc[image][mappings.UTIS_X],
-                height=mappings.GK_HEIGHT_BASE + utis_data.loc[image][mappings.UTIS_Y],
+            utis_lat, utis_long = convert_field_coordinate_to_wgs84(
+                east_coordinate_m=utis_data.loc[image][mappings.UTIS_X],
+                north_coordinate_m=utis_data.loc[image][mappings.UTIS_Y],
             )
             calibration_properties_data = {
                 mappings.MOTOR_POS_KEY: {
