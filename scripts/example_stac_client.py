4f53c63c514133571e21a594703e60d5547a6984
Kaleb Phipps <kaleb.phipps@kit.edu>
2024-12-10 14:09:38 +0100

    make executable

--- scripts/example_stac_client.py
+++ scripts/example_stac_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+from datetime import datetime
 
 import paint.util.paint_mappings as mappings
 from paint import PAINT_ROOT
@@ -79,14 +80,14 @@ if __name__ == "__main__":
     client = StacClient(output_dir=args.output_dir)
 
     # Download tower measurements.
-    # client.get_tower_measurements()
-    #
-    # # Download weather data between a certain time period.
-    # client.get_weather_data(
-    #     data_sources=args.weather_data_sources,
-    #     start_date=datetime.strptime(args.start_date, mappings.TIME_FORMAT),
-    #     end_date=datetime.strptime(args.end_date, mappings.TIME_FORMAT),
-    # )
+    client.get_tower_measurements()
+
+    # Download weather data between a certain time period.
+    client.get_weather_data(
+        data_sources=args.weather_data_sources,
+        start_date=datetime.strptime(args.start_date, mappings.TIME_FORMAT),
+        end_date=datetime.strptime(args.end_date, mappings.TIME_FORMAT),
+    )
 
     # Download heliostat data.
     client.get_heliostat_data(
@@ -97,4 +98,4 @@ if __name__ == "__main__":
 
     # Download metadata for all heliostats.
     # WARNING: Running the following line with `heliostats=None` will take a very long time!
-    # client.get_heliostat_metadata(heliostats=None)
+    client.get_heliostat_metadata(heliostats=None)
